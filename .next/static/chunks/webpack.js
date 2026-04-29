df8fb248297650017b7325578aaea72e95d41950
Singh <sumit_singh43@csu.fullerton.edu>
2026-04-28 18:37:32 -0700

    Added admin login page, added card view in Event Management

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af61043ab5c5b75a"; }
+/******/ 		__webpack_require__.h = function() { return "40f8f175a16db044"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
